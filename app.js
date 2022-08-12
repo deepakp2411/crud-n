0128360823c66b7941f293c4bbafc3cfcd7c20c4
Deepak <bbaz2deepak@gmail.com>
2022-08-12 15:56:14 +0530

    added some files

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
 // connect db
 connectDB(DATABASE_URL);
 
+app.use(express.urlencoded({extended:false}))
+
 // static files
 
 app.use('/student',express.static(join(process.cwd(), "public")));
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,33 +1,65 @@
 import StudentModel from "../models/Student.js";
 
 class StudentController {
-  static createDoc = (req, res) => {
-    res.redirect("/student");
+  static createDoc = async (req, res) => {
+    try {
+      const { name, age, fees } = req.body;
+      const doc = new StudentModel({
+        name: name,
+        age: age,
+        fees: fees,
+      });
+      // saving doc
+      const result = await doc.save();
+      res.redirect("/student");
+    } catch (error) {
+      console.log(error);
+    }
   };
   // get all doc
   static getAllDoc = async (req, res) => {
     try {
       const result = await StudentModel.find();
-      console.log(result);
-      res.render("index", { data:result });
+      // console.log(result);
+      res.render("index", { data: result });
     } catch (error) {
       console.log(error);
     }
   };
 
-  //   edit
-  static editDoc = (req, res) => {
-    res.render("edit");
+  //   edit doc
+  static editDoc = async (req, res) => {
+    try {
+      const result = await StudentModel.findById(req.params.id);
+      res.render("edit", { data: result });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  //   update
-  static updateDoc = (req, res) => {
-    res.redirect("/student");
+  //   update doc
+  static updateDoc = async (req, res) => {
+    try {
+      const result = await StudentModel.findByIdAndUpdate(
+        req.params.id,
+        req.body
+      );
+
+      res.redirect("/student");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  //   delete
-  static deleteDoc = (req, res) => {
-    res.redirect("/student");
+  //   delete doc
+  static deleteDoc = async (req, res) => {
+    try {
+      const result = await StudentModel.findByIdAndDelete(req.params.id);
+
+      res.redirect("/student");
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
